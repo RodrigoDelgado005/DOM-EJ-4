a91365b016c226895d6d3c1d5c81c030c90c6249
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-11 12:15:31 -0300

    first commit

---
+++ JS/function.js
@@ -0,0 +1,14 @@
+function cantidadHijos() {
+    let objeto1 = document.getElementById('bloque1')
+    alert('La cantidad de hijos del nodo div es:' + objeto1.childNodes.length)
+    let objeto2 = document.getElementById('bloque2')
+    alert('La cantidad de hijos del nodo div es:' + objeto2.childNodes.length)
+    
+}
+
+function cantidadHijosElemento() {
+    let objeto1 = document.getElementById('bloque1')
+    alert('La cantidad de hijos del nodo div es:' + objeto1.children.length)
+    let objeto2 = document.getElementById('bloque2')
+    alert('La cantidad de hijos del nodo div es:' + objeto2.children.length)
+}
